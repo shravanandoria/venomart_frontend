2a3baece9482cc2382c3c6db6fe312ad399156df
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-07-28 08:57:43 +0530

    initiated wallet connect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,53 +1,85 @@
-// def 
-import { useEffect, useState } from 'react'
+// def
+import { useEffect, useState } from "react";
 
-// components 
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+// components
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-// sty;es 
-import '@/styles/globals.css'
-import '@/styles/custom.css'
-import '@/styles/tailwind.css'
-import '@/styles/Home.module.css'
+// sty;es
+import "@/styles/globals.css";
+import "@/styles/custom.css";
+import "@/styles/tailwind.css";
+import "@/styles/Home.module.css";
 
-// images 
+// images
 import testNFT from "../../public/test.jpg";
 
+//Wallet Connect
+import VenomConnect from "venom-connect";
+import { initVenomConnect } from "@/utils/wallet_connect";
+import {
+  onConnect,
+  checkAuth,
+  getAddress,
+  onDisconnect,
+  onProviderReady,
+} from "@/utils/wallet_info";
 
 export default function App({ Component, pageProps }) {
-
   const blockURL = "https://venomart.space/";
   const signer_address = "ox44";
   const defaultCollectionAddress = "ox44";
   const defTheme = "dark";
   const [theme, setTheme] = useState(defTheme);
+  const [venomConnect, setVenomConnect] = useState();
 
-  // test collection array 
-  const all_collections = [{
-    Cover: testNFT,
-    Logo: testNFT,
-    Name: "cover",
-    OwnerAddress: "cover",
-    CollectionAddress: "cover",
-  }]
+  // test collection array
+  const all_collections = [
+    {
+      Cover: testNFT,
+      Logo: testNFT,
+      Name: "cover",
+      OwnerAddress: "cover",
+      CollectionAddress: "cover",
+    },
+  ];
 
-  // test collection array 
-  const all_nfts = [{
-    ImageSrc: testNFT,
-    Name: "nft",
-    Description: "test descrip",
-    Address: "cover",
-    tokenId: 1,
-    listedBool: true,
-    listingPrice: 2,
-  }]
+  // test collection array
+  const all_nfts = [
+    {
+      ImageSrc: testNFT,
+      Name: "nft",
+      Description: "test descrip",
+      Address: "cover",
+      tokenId: 1,
+      listedBool: true,
+      listingPrice: 2,
+    },
+  ];
+  const init = async () => {
+    const _venomConnect = await initVenomConnect();
+    setVenomConnect(_venomConnect);
+  };
 
-  // setting website theme 
+  // setting website theme
   useEffect(() => {
     const defThemeLocal = localStorage.getItem("WebsiteTheme");
     setTheme(defThemeLocal);
-  }, [])
+    init();
+  }, []);
+
+  useEffect(() => {
+    // connect event handler
+    const off = venomConnect?.on("connect", onConnect);
+    if (venomConnect) {
+      checkAuth(venomConnect);
+    }
+    // just an empty callback, cuz we don't need it
+    return () => {
+      off?.();
+    };
+  }, [venomConnect]);
+
   return (
     <>
       <Navbar
@@ -61,11 +93,10 @@ export default function App({ Component, pageProps }) {
         signer_address={signer_address}
         defaultCollectionAddress={defaultCollectionAddress}
         blockURL={blockURL}
-
         all_collections={all_collections}
         all_nfts={all_nfts}
       />
       <Footer theme={theme} />
     </>
-  )
+  );
 }
---
+++ src/utils/wallet_info.js
@@ -0,0 +1,26 @@
+export const getAddress = async (provider) => {
+  const providerState = await provider?.getProviderState?.();
+  return providerState?.permissions.accountInteraction?.address.toString();
+};
+// Any interaction with venom-wallet (address fetching is included) needs to be authentificated
+export const checkAuth = async (_venomConnect) => {
+  const auth = await _venomConnect?.checkAuth();
+  if (auth) await getAddress(_venomConnect);
+};
+// This handler will be called after venomConnect.login() action
+// connect method returns provider to interact with wallet, so we just store it in state
+export const onConnect = async (provider) => {
+  setVenomProvider(provider);
+  await onProviderReady(provider);
+};
+// This handler will be called after venomConnect.disconnect() action
+// By click logout. We need to reset address and balance.
+export const onDisconnect = async () => {
+  venomProvider?.disconnect();
+  return undefined;
+};
+// When our provider is ready, we need to get address and balance from.
+export const onProviderReady = async (provider) => {
+  const venomWalletAddress = provider ? await getAddress(provider) : undefined;
+  return venomWalletAddress;
+};
