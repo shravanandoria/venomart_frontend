040b8d0d9378691da64f528b5f2cbb63908df57a
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-07-31 11:41:14 +0530

    added user to database function

--- src/lib/dbConnect.jsx
+++ src/lib/dbConnect.jsx
@@ -1,38 +1,41 @@
 import mongoose from "mongoose";
-
-const mongodb_uri = process.env.NEXT_PUBLIC_Mongo_URI;
+// 123Snaahaha
+const mongodb_uri =
+  "mongodb+srv://shravanandoria21437:123Snaahaha@cluster0.xdumlze.mongodb.net/?retryWrites=true&w=majority";
+// "mongodb+srv://venomartspace:anu@2000@venom-testnet.niuxzx8.mongodb.net/?retryWrites=true&w=majority";
 // const mongodb_uri = "mongodb://localhost:27017";
 
 if (!mongodb_uri) {
-    throw new error("missing uri");
+  throw new error("missing uri");
 }
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-
-    if (!cached.promise) {
-        const opts = {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // bufferCommands: false,
-            // bufferMaxEntries: 0,
-            // useFindAndModify: true,
-            // useCreateIndex: true,
-        };
-        cached.promise = mongoose.connect(mongodb_uri, opts).then((mongoose) => {
-            return mongoose;
-        });
-    }
-    cached.conn = await cached.promise;
+  if (cached.conn) {
     return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // bufferCommands: false,
+      // bufferMaxEntries: 0,
+      // useFindAndModify: true,
+      // useCreateIndex: true,
+    };
+    cached.promise = mongoose.connect(mongodb_uri, opts).then((mongoose) => {
+      console.log("wallet connected");
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default dbConnect;
