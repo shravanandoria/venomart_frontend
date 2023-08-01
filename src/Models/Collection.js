a531ebee7304649399d07f039fae7ce2eadee53b
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-08-01 20:39:41 +0530

    bug fix

--- src/Models/Collection.js
+++ src/Models/Collection.js
@@ -1,18 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-const CollectionSchema = new mongoose.Schema({
-  contractAddress: {
-    type: String,
-    unique: true,
+const CollectionSchema = new mongoose.Schema(
+  {
+    contractAddress: {
+      type: String,
+      unique: true,
+    },
+    creatorAddress: String,
+    coverImage: String,
+    logo: String,
+    name: String,
+    royalty: String,
+    description: String,
+    isVerified: Boolean,
   },
-  creatorAddress: String,
-  coverImage: String,
-  logo: String,
-  name: String,
-  royalty: String,
-  description: String,
-  isVerified: Boolean,
-});
+  { timestamps: true }
+);
 
 module.exports =
   mongoose.models?.Collection || mongoose.model("Collection", CollectionSchema);
--- src/pages/api/collection/collection.js
+++ src/pages/api/collection/collection.js
@@ -1,6 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
-import Collection from "../../../Models/Collection";
 import User from "../../../Models/User";
+import Collection from "../../../Models/Collection";
 
 export default async function handler(req, res) {
   const { method } = req;
