38d21abfdbe036069ed121662154de64098caa2b
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-08-19 10:39:19 +0530

    fixing imports

--- src/pages/api/collection/slug_collection.js
+++ src/pages/api/collection/slug_collection.js
@@ -1,4 +1,4 @@
-import dbConnect from "@/lib/dbConnect";
+import dbConnect from "../../../lib/dbConnect";
 import Collection from "../../../Models/Collection";
 
 export default async function handler(req, res) {
