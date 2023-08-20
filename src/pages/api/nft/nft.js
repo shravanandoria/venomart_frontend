be659790dc5600e8d97a984343a2da198c249d19
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-08-20 23:22:51 +0530

    added nft api

--- src/pages/api/nft/nft.js
+++ src/pages/api/nft/nft.js
@@ -10,29 +10,75 @@ export default async function handler(req, res) {
     // GET NFT BY NFT ADDRESS
     case "GET":
       try {
-        const { NFTAddress } = req.query;
-        let nft = await NFT.findOne({ NFTAddress });
-        if (!nft)
-          return res
-            .status(400)
-            .json({ success: false, data: "Cannot Find This NFT" });
+        const { page, NFTAddress, ownerAddress } = req.query;
+        let contentPerPage = 9;
 
-        res.status(200).json({ success: false, data: nft });
+        // GET USER'S NFTS
+        if (ownerAddress) {
+          let nfts = await NFT.find({ ownerAddress })
+            .skip(page * contentPerPage)
+            .limit(contentPerPage);
+          return res.status(200).json({ success: false, data: nfts });
+        }
+
+        // IF NFT ADDRESS IS PROVIDED, SEND THAT NFT
+        if (NFTAddress) {
+          let nft = await NFT.findOne({ NFTAddress })
+            .skip(page * contentPerPage)
+            .limit(contentPerPage);
+
+          if (!nft)
+            return res
+              .status(400)
+              .json({ success: false, data: "Cannot Find This NFT" });
+
+          return res.status(200).json({ success: false, data: nft });
+        }
+
+        //SEND ALL NFTS
+        let nfts = await NFT.find()
+          .skip(page * contentPerPage)
+          .limit(contentPerPage);
+        res.status(200).json({ success: false, data: nfts });
       } catch (error) {
-        res.status(400).json({ success: false });
+        res.status(400).json({ success: false, data: error.message });
       }
       break;
 
     //CREATE NEW NFT
     case "POST":
       try {
-        let nft = await NFT.findOne({ NFTAddress: req.body.NFTAddress });
+        const {
+          NFTAddress,
+          ownerAddress,
+          managerAddress,
+          imageURL,
+          title,
+          description,
+          properties,
+          NFTCollection,
+        } = req.body;
+
+        let nft = await NFT.findOne({ NFTAddress });
         if (nft)
           return res
             .status(400)
             .json({ success: false, data: "This nft already exists" });
 
-        nft = await NFT.create({ ...req.body });
+        nft = await NFT.create({
+          NFTAddress,
+          ownerAddress,
+          managerAddress,
+          imageURL,
+          title,
+          description,
+          isListed: false,
+          isLike: false,
+          listingPrice: 0,
+          properties,
+          NFTCollection,
+          transactions: [],
+        });
 
         res.status(200).json({ success: true, data: nft });
       } catch (error) {
