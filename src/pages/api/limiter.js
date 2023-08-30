2577d42de5bc3075f027b3bb8e1b1279b3fc995d
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-08-30 16:19:33 +0530

    rate limit changes

--- src/pages/api/limiter.js
+++ src/pages/api/limiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Max requests per windowMs
+    max: 1000000, // Max requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
 });
 
